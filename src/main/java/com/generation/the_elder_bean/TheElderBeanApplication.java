cc97df0d5f92c0a4d068feb943eb9d81e5a80ecf
dario <antoniazzidario26@gmail.com>
2025-01-16 16:28:45 +0100

    versione 0

---
+++ src/main/java/com/generation/the_elder_bean/TheElderBeanApplication.java
@@ -0,0 +1,15 @@
+package com.generation.the_elder_bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheElderBeanApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(TheElderBeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/the_elder_bean/TheElderBeanApplicationTests.java
@@ -0,0 +1,15 @@
+package com.generation.the_elder_bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheElderBeanApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
